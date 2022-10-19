a1cd6d2b226cd0736055e581bfa5c3758603deb1
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-19 16:42:51 +0900

    회원 서비스 테스트 코드

---
+++ src/test/java/hello/myCore/member/MemberServiceTest.java
@@ -0,0 +1,18 @@
+package hello.myCore.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService =  new MemberServiceImpl();
+    @Test
+    void join() {
+        Member member = new Member(1L, "memberA", Grade.Vip);
+
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
